aca9e44061a6472f2ad6ed829f61ae36a6bb9c0d
Rasmus AA <rasmus-baunvig@live.dk>
2023-11-07 14:25:14 +0100

    Added book to main program

--- SOLID/SOLID/Interface Segregation Principle/Program.cs
+++ SOLID/SOLID/Interface Segregation Principle/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            IBorrowableBook book = new Book();
+            IBook book2 = new Book();
+
+            book.CheckOut("Peter");
+            Console.WriteLine(book.GetDueDate());
+
+            // Book2 is not borrowable, cant be checked out...
+            //book2.CheckOut("Peter");
         }
     }
 }
