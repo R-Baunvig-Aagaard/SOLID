ec949bd35d58e37144d49e4aabe98deff3a001de
Rasmus AA <rasmus-baunvig@live.dk>
2023-11-07 13:53:00 +0100

    ReferenceBook class updated with IBook interface

--- SOLID/SOLID/Interface Segregation Principle/ReferenceBook.cs
+++ SOLID/SOLID/Interface Segregation Principle/ReferenceBook.cs
@@ -6,29 +6,14 @@ using System.Threading.Tasks;
 
 namespace Interface_Segregation_Principle
 {
-    public class ReferenceBook : ILibraryItem
+    public class ReferenceBook : IBook
     {
         public string Title { get; set; }
         public string Author { get; set; }
-        public DateTime BorrowDate { get; set; }
-        public string Borrower { get; set; }
-        public int CheckOutDurationInDays { get; set; } = 0;
+        
         public string LibraryId { get; set; }
         public int Pages { get; set; }
 
-        public void CheckIn()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CheckOut(string borrower)
-        {
-            throw new NotImplementedException();
-        }
-
-        public DateTime GetDueDate()
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
